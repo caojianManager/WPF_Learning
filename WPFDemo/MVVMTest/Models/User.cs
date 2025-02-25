0e5938ccb1dc2a7bdf68ad101f9c042273fecc77
CAOJIAN\Cao Jian <caojianmanager@163.com>
2025-02-25 17:21:10 +0800

    add demo

---
+++ WPFDemo/MVVMTest/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTest.Models
+{
+    public class User
+    {
+        public string? Email { get; set; }
+        public string? Name { get; set; }
+    }
+
+    public static class UserManager
+    {
+        public static ObservableCollection<User> DataBaseUsers = new ObservableCollection<User>()
+         {
+             new User() { Name = "小王", Email = "123@qq.com" },
+             new User() { Name = "小红", Email = "456@qq.com" },
+             new User() { Name = "小五", Email = "789@qq.com" }
+         };
+
+        public static ObservableCollection<User> GetUsers()
+        {
+            return DataBaseUsers;
+        }
+
+        public static void AddUser(User user)
+        {
+            DataBaseUsers.Add(user);
+        }
+    }
+}
---
+++ WPFDemo/MVVMTest/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using MVVMTest.Commands;
+using MVVMTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMTest.ViewModel
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<User> Users;
+        public ICommand AddUserCommand { get; set; }
+
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
+        public MainViewModel() 
+        {
+            Users = UserManager.DataBaseUsers;
+            AddUserCommand = new RelayCommand(AddUser,CanAddUser);
+        
+        }
+
+        private bool CanAddUser(object obj)
+        {
+            return true;
+        }
+
+        private void AddUser(object obj)
+        {
+            User user = new User();
+            user.Name = Name;
+            user.Email = Email;
+            UserManager.AddUser(user);
+        }
+
+    }
+}
