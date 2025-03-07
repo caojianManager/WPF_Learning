710b53131c5a1f61750cd937e7ae1225f4b4cb9f
caojian <caojianmanager@163.com>
2025-03-07 14:47:46 +0800

    add viewModel

--- WPFDemo/WPFDemo/ViewModels/MainWindowViewModel.cs
+++ WPFDemo/WPFDemo/ViewModels/MainWindowViewModel.cs
@@ -7,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace WPFDemo.ViewModels
 {
-    class MainWindowViewModel:ViewModelBase
+    class MainWindowViewModel : ViewModelBase
     {
+        private bool _isReadOnly = false;
+        public bool IsReadOnly {
+            get => _isReadOnly;
+            set => SetProperty(ref _isReadOnly, value);
+        }
+
+        private bool _isTitleBarVisible = false;
+        public bool IsTitleBarVisible
+        {
+            get => _isTitleBarVisible;
+            set => SetProperty(ref _isTitleBarVisible, value);
+        }
+    
     }
 }
