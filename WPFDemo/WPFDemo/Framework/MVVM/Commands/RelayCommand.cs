e1adb7c6d879a8834905e2723166deca7a3aa89a
caojian <caojianmanager@163.com>
2025-03-12 16:47:29 +0800

    update FrameWork

---
+++ WPFDemo/WPFDemo/Framework/MVVM/Commands/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrinterSendMessage.Framework.MVVM
+{
+    class RelayCommand : Command
+    {
+        private Action<object> _Excute {  get; set; }
+        private Predicate<object> _CanExcute { get; set; }
+
+        public RelayCommand(Action<object> excute, Predicate<object> canExcute)
+        {
+            _Excute = excute;
+            _CanExcute = canExcute;
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return _CanExcute(parameter);
+        }
+
+        public override void Execute(object parameter)
+        {
+            _Excute(parameter);
+        }
+    }
+}
