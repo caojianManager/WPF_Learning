5c0134ae388bd6d3d962032125f5f36a945bdff0
caojian <caojianmanager@163.com>
2025-03-12 18:00:02 +0800

    update info

--- WPFDemo/WPFDemo/Framework/WindowsManager/WindowManager.cs
+++ WPFDemo/WPFDemo/Framework/WindowsManager/WindowManager.cs
@@ -4,9 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WPFDemo.Framework.WindowsManager
+namespace Framework.WindowsManager
 {
-    class WindowManager
+    using System.Windows;
+
+    public class WindowManager : IWindowManager
     {
+        public void ShowWindow(object viewModel)
+        {
+            // 找到视图并显示窗口
+            var view = ViewLocator.LocateForModel(viewModel);
+            if (view is Window window)
+            {
+                window.Show();
+            }
+        }
+
+        public void CloseWindow(object viewModel)
+        {
+            var view = ViewLocator.LocateForModel(viewModel);
+            if (view is Window window)
+            {
+                window.Close();
+            }
+        }
+
+        public void ActivateWindow(object viewModel)
+        {
+            var view = ViewLocator.LocateForModel(viewModel);
+            if (view is Window window)
+            {
+                window.Activate();
+            }
+        }
+
+        public void ShowDialog(object viewModel)
+        {
+            var view = ViewLocator.LocateForModel(viewModel);
+            if (view is Window window)
+            {
+                window.ShowDialog();
+            }
+        }
     }
+
 }
